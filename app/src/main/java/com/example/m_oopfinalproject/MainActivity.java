89e3f3bed9c2c452374dbf851416f37f2c37b732
Gregorius Albert <galbert.djaja@gmail.com>
2020-05-20 20:35:11 +0700

    Button functions

--- app/src/main/java/com/example/m_oopfinalproject/MainActivity.java
+++ app/src/main/java/com/example/m_oopfinalproject/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.m_oopfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toRandom(View view) {
+        Intent intent = new Intent(this, RandomActivity.class);
+        startActivity(intent);
+    }
+    public void toPhytagoras(View view) {
+        Intent intent = new Intent(this, PhytagorasActivity.class);
+        startActivity(intent);
+    }
+    public void toQuadratic(View view) {
+        Intent intent = new Intent(this, QuadraticActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/m_oopfinalproject/RandomActivity.java
+++ app/src/main/java/com/example/m_oopfinalproject/RandomActivity.java
@@ -2,7 +2,9 @@ package com.example.m_oopfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RandomActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RandomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+    }
+
+    public void toHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
