c040ab10510175b7ab1d13552fd68422cc7a35f2
yonat <yonathank28@gmail.com>
2020-05-20 21:07:45 +0700

    update kevin and andre

--- app/src/main/java/com/example/m_oopfinalproject/MainActivity.java
+++ app/src/main/java/com/example/m_oopfinalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.m_oopfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void phytagorasButton(View view){
+        Intent intent2 = new Intent(this, PhytagorasActivity.class);
+
+        startActivity(intent2);
     }
 }
